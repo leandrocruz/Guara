47f8d049fea938d091c158f83dc35a22c28e2354
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2012-11-26 14:14:07 -0200

    * Script update

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 
 call git pull
 
-mvn -Dmaven.test.failure.ignore=true clean install
-
 mvn -Declipse.workspace=C:\users\alexandre\dev\eclipse\ws-kidux eclipse:eclipse
+
+REM mvn -Dmaven.test.failure.ignore=true clean install
+mvn clean install
 
